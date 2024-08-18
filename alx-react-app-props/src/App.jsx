1756ecff6f24fe427402e7989cfff53b698d0974
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-08-19 00:25:33 +0100

    fix

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,13 @@
+// src/App.jsx
 import React from 'react';
-import ProfilePage from './components/UserProfile';
-import UserContext from './components/UserContext.js';
+import ProfilePage from './ProfilePage';
+import { UserProvider } from './context/UserContext';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
   return (
-    <UserContext.Provider value={userData}>
+    <UserProvider>
       <ProfilePage />
-    </UserContext.Provider>
+    </UserProvider>
   );
 }
 
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import UserInfo from './UserInfo';
-import { UserContext } from './UserContext.js'
+// src/components/UserProfile.jsx
+import React, { useContext } from 'react';
+import { UserContext } from '../context/UserContext'; // Adjust the path as necessary
 
-function ProfilePage() {
-  return <UserInfo />;
+function UserProfile() {
+  // Use the context
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default ProfilePage;
+export default UserProfile;
