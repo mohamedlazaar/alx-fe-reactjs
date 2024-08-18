58b982b975ef9eeb354aa17f58480b685c198c8c
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-08-18 23:05:56 +0100

    styling the header maincontent and footer components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return (
+        <footer style ={{ border: '1px solid gray', padding: '10px', margin: '10px', backgroundColor: 'black' }}>
+           <p style={{ color: 'white' }}>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+export default Footer;
