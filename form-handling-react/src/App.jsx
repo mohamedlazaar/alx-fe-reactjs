34129df19815101aafa27c70baee57c0cf62afca
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-09-01 22:33:25 +0100

    fixing some issues

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
 
 function App() {
   return (
     <div className="App">
-      <h1>User Registration</h1>
-      <RegistrationForm />
+      <FormikForm />
     </div>
   );
 }
