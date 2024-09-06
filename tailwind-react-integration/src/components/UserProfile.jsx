b5d3681f7216a04cc4f05731ae1f06d20a64aa39
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-09-06 21:22:49 +0100

    adding responsive to the design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
     return (
-      <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-        <img src="https://via.placeholder.com/150" className="rounded-full w-36 h-36 mx-auto" alt="User" />
-        <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <div className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+        <img src="https://via.placeholder.com/150" className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto" alt="User" />
+        <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+        <p className="text-gray-600 md:text-base sm:text-sm ">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
