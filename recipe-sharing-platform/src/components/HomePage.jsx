4c6b3907d4fe06acf817db033e03b562daa0e720
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-09-13 21:19:10 +0100

    add hover to the homepage style

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = ()=>{
             <h1 className="text-center text-4xl font-bold text-green-500">Recipes</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 my-5">
                 {recipes.map((recipe)=>(
-                    <div key={recipe.id} className="bg-green-400 flex flex-col justify-center items-center py-6">
+                    <div key={recipe.id} className="bg-green-400 flex flex-col justify-center items-center py-6 shadow-lg hover:bg-green-600">
                         <img src={recipe.image} alt={recipe.title} className="w-full h-32 object-cover rounded" />
                         <h2 className="text-xl font-semibold mt-4">{recipe.title}</h2>
                         <p className="text-gray-600 mt-2">{recipe.summary}</p>
