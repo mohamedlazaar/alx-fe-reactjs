09af8f35ad124bba68902ab9ce7570d068211185
mohamedlazaar <45826424+mohamedlazaar@users.noreply.github.com>
2024-09-23 12:08:02 +0100

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,10 +3,22 @@ import axios from 'axios';
 
 const SEARCH_URL = 'https://api.github.com/search/users?q=';
 
-export const fetchGitHubUsers = async (query) => {
+export const fetchGitHubUsers = async (query, location = '', minRepos = 0) => {
   const apiKey = import.meta.env.VITE_GITHUB_API_KEY;
+  
+  // Construct the search query
+  let searchQuery = query;
+
+  if (location) {
+    searchQuery += `+location:${location}`;
+  }
+
+  if (minRepos) {
+    searchQuery += `+repos:>=${minRepos}`;
+  }
+
   try {
-    const response = await axios.get(`${SEARCH_URL}${query}`, {
+    const response = await axios.get(`${SEARCH_URL}${searchQuery}`, {
       headers: {
         Authorization: `token ${apiKey}`, // Use your GitHub API key
       },
