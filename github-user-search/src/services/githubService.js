8e3678058a2e6a02ad9cc7ec611ebe3c68059545
mohamedlazaar <45826424+mohamedlazaar@users.noreply.github.com>
2024-09-23 12:06:51 +0100

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,19 @@
+// src/services/githubService.js
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/users';
+const SEARCH_URL = 'https://api.github.com/search/users?q=';
 
-export const fetchUserData = async (username) => {
-  const apiKey = import.meta.env.VITE_GITHUB_API_KEY; // Use environment variable
-  const response = await axios.get(`${BASE_URL}/${username}`, {
-    headers: {
-      Authorization: `token ${apiKey}`,
-    },
-  });
-  return response.data;
+export const fetchGitHubUsers = async (query) => {
+  const apiKey = import.meta.env.VITE_GITHUB_API_KEY;
+  try {
+    const response = await axios.get(`${SEARCH_URL}${query}`, {
+      headers: {
+        Authorization: `token ${apiKey}`, // Use your GitHub API key
+      },
+    });
+    return response.data.items; // `items` contains an array of user objects
+  } catch (error) {
+    console.error('Error fetching GitHub users:', error);
+    return [];
+  }
 };
